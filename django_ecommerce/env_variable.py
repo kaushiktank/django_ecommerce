f036698ec7091c6ea343629ad5d69d12ddb25a67
kaushiktank <31559404+kaushiktank@users.noreply.github.com>
2022-01-10 11:37:35 +0530

    Delete env_variable.py

--- django_ecommerce/env_variable.py
+++
@@ -1,7 +0,0 @@
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'tankkaushik11130@gmail.com'
-EMAIL_HOST_PASSWORD = 'xekrsbunehfmtksw'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FORM_EMAIL = 'tankkaushik11130@gmail.com'
